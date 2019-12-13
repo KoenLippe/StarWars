3d2d7d6e1591d3ada6ad1a34bed4dcd9d72da463
Koen Lippe <koen.lippe@quicknet.nl>
2019-12-13 19:56:11 +0100

    Forgot to add jquery & popper.js for bootstrap use

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './home/navbar/navbar';
 import Categories from './home/main/categories/categories';
